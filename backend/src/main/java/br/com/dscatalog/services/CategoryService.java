93041971bce6839c1039595a9f82fe8c24cdbbae
torezanidev <torezanidev@gmail.com>
2024-05-20 15:22:30 -0300

    DTO

--- backend/src/main/java/br/com/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package br.com.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.dscatalog.dto.CategoryDTO;
 import br.com.dscatalog.entities.Category;
 import br.com.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +17,11 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
